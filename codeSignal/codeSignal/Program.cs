cfbcb81a91c55f2fb4a45c0d13acb24f529369d1
Brahimhz <b.bouhadida@univ-boumerdes.dz>
2020-07-05 14:37:55 +0100

    largestNumber

--- codeSignal/codeSignal/Program.cs
+++ codeSignal/codeSignal/Program.cs
@@ -8,7 +8,13 @@ namespace codeSignal
         static void Main(string[] args)
         {
             TheCore = new TheCore();
-            Console.WriteLine(TheCore.addTwoDigits(29));
+
+            //Console.WriteLine(TheCore.addTwoDigits(29));
+
+            Console.WriteLine(TheCore.largestNumber(2));
+
+            Console.ReadLine();
+
 
         }
     }
--- codeSignal/codeSignal/TheCore.cs
+++ codeSignal/codeSignal/TheCore.cs
@@ -1,10 +1,17 @@
-﻿namespace codeSignal
+﻿using System;
+
+namespace codeSignal
 {
     class TheCore
     {
         public int addTwoDigits(int n)
         {
             return (n / 10) + (n % 10);
+        }
+
+        public int largestNumber(int n)
+        {
+            return (int)((Math.Pow(10 , n )) - 1);
         }
     }
 }
