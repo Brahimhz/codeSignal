0f74bfa2af09ba2bbead2dc602271f9f1013ac06
Brahimhz <b.bouhadida@univ-boumerdes.dz>
2020-07-14 17:59:21 +0100

    phoneCall

--- codeSignal/codeSignal/Program.cs
+++ codeSignal/codeSignal/Program.cs
@@ -19,9 +19,9 @@ namespace codeSignal
 
             //Console.WriteLine(TheCore.circleOfNumbers(10,7));
 
-            Console.WriteLine(TheCore.lateRide(808));
+            //Console.WriteLine(TheCore.lateRide(808));
 
-
+            Console.WriteLine(TheCore.phoneCall(1,2,1,6));
 
             Console.ReadLine();
 
--- codeSignal/codeSignal/TheCore.cs
+++ codeSignal/codeSignal/TheCore.cs
@@ -29,5 +29,44 @@ namespace codeSignal
         {
             return ((n / 60) / 10) + ((n / 60) % 10) + ((n % 60) / 10) + ((n % 60) % 10);
         }
+
+        public int phoneCall(int min1, int min2_10, int min11, int s)
+        {
+            var count = 0;
+            var nbrMin = 0;
+
+            if (s < min1)
+                return count;
+            else
+            {
+                count++;
+                s -= min1;
+                if (s < min2_10)
+                    return count;
+                else
+                {
+                    nbrMin = s / min2_10;
+                    if (nbrMin > 9)
+                    {
+                        count += 9;
+                        s -= 9 * min2_10;
+                    }
+                    else
+                    {
+                        count += s / min2_10;
+                        s -= (s / min2_10) * min2_10;
+                    }
+                }
+
+               if (s < min11 || nbrMin <= 9)
+                        return count;
+                else
+                 {
+                        count += s / min11;
+                        return count;
+                 }
+                
+            }
+        }
     }
 }
